e0dded7041522362e9a4ff9c746c4a320bc94ba6
bin.zhang <bin.zhang@shuyun.com>
2017-05-25 22:34:44 +0800

    init: add project to github

---
+++ src/main/java/cn/itcast/core/controller/CenterController.java
@@ -0,0 +1,14 @@
+package cn.itcast.core.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * 后台管理
+ * 测试
+ * @author lx
+ *
+ */
+@Controller
+public class CenterController {
+
+}
